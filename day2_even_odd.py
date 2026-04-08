7b20c4e68fe48cd9d40bf51c05169ffe9ef90857
Nithya-99720 <bl.ai.u4aid24050bl@.students.amrita.edu>
2026-04-08 06:49:02 +0530

    Day 2 conditions practice

---
+++ day2_even_odd.py
@@ -0,0 +1,7 @@
+num=int(input("Enter number:"))
+
+if num % 2 ==0:
+    print("Number:",num," is Even") 
+
+else:
+    print("Number:",num,"is Odd")
---
+++ day2_largest.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+
+if a>=b and a>=c:
+    print("A is largest")
+
+elif b>=a and b>=c:
+    print("B is largest")
+else:
+    print("C is largest")
+    
